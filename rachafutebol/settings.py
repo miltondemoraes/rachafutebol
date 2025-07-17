206bb0c039067c0135f436673a82d4175510f7e5
Jose Milton <jmmsn27@gmail.com>
2025-07-17 19:00:38 -0300

    fix: settings

--- rachafutebol/settings.py
+++ rachafutebol/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z9bften&o5*lge48dcimn&py$e@=^gc02d(x^dzt4m%g-ecdw-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rachafutebol.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
